8852b0e3cfc43bd3cc9b1bd7f914bb51d75a52ec
Lauv <315085251@qq.com>
2022-08-09 21:31:19 +0800

    fix: card move

--- src/card.tsx
+++ src/card.tsx
@@ -13,15 +13,35 @@ export class Card extends godot.Area2D {
     (this.get_node('Control/Header/Title') as godot.Label).set_text(text)
   }
 
-  public _input_event(_viewport: Object, event: godot.InputEvent, _shape_idx: number) {
+  public _unhandled_input(event: godot.InputEvent) {
     if (event instanceof godot.InputEventMouseButton && event.button_index === godot.BUTTON_LEFT) {
-      this.get_tree().set_input_as_handled()
-      this.canGrab = event.pressed
-      // @ts-expect-error vector2
-      this.grabbedOffset = this.position - this.get_global_mouse_position()
+      // this.canGrab = event.pressed
+      if (this.canGrab) {
+        this.get_tree().set_input_as_handled()
+        // @ts-expect-error vector2
+        this.grabbedOffset = this.position - this.get_global_mouse_position()
+      }
       console.log('canGrab', this.canGrab)
     }
   }
+
+  public _on_Card_mouse_entered() {
+    this.canGrab = true
+  }
+
+  public _on_Card_mouse_exited() {
+    this.canGrab = false
+  }
+
+  // public _input_event(_viewport: Object, event: godot.InputEvent, _shape_idx: number) {
+  //   if (event instanceof godot.InputEventMouseButton && event.button_index === godot.BUTTON_LEFT) {
+  //     this.get_tree().set_input_as_handled()
+  //     this.canGrab = event.pressed
+  //     // @ts-expect-error vector2
+  //     this.grabbedOffset = this.position - this.get_global_mouse_position()
+  //     console.log('canGrab', this.canGrab)
+  //   }
+  // }
 
   public _process() {
     if (godot.Input.is_mouse_button_pressed(godot.BUTTON_LEFT) && this.canGrab) {
