ef0d636b04f1d579ae9ad7ce4ad0fa2edb51841b
wengxiangmin <157215725@qq.com>
2022-08-13 00:07:30 +0800

    refactor: 调整卡片组件

--- src/card.tsx
+++ src/card.tsx
@@ -1,30 +1,37 @@
 import { vector2 } from './utils'
 
-export class Card extends godot.Node2D {
-  public grabbedOffset = vector2()
-  public canGrab = false
+export class Card extends godot.RigidBody2D {
+  public dragging = false
+  public movement = vector2()
+  public area?: godot.Area2D
+  public _position?: number
+
+  _ready() {
+    this.area = this.get_node('area') as godot.Area2D
+  }
 
   public _on_view_gui_input(event: godot.InputEvent) {
     if (event instanceof godot.InputEventMouseButton && event.button_index === godot.BUTTON_LEFT) {
-      this.get_tree().set_input_as_handled()
-      this.canGrab = event.pressed
+      this.dragging = event.pressed
       // @ts-expect-error vector2
-      this.grabbedOffset = this.position - this.get_global_mouse_position()
+      this.movement = this.position - this.get_global_mouse_position()
+      this.get_tree().set_input_as_handled()
     }
   }
 
   public _process() {
-    if (godot.Input.is_mouse_button_pressed(godot.BUTTON_LEFT) && this.canGrab) {
-      this.position = this.get_global_mouse_position() + this.grabbedOffset
+    if (godot.Input.is_mouse_button_pressed(godot.BUTTON_LEFT) && this.dragging) {
+      this.position = this.get_global_mouse_position() + this.movement
+    } else if (this._position) {
+      this.position = this._position
     }
   }
 
-  public _on_area_body_entered(area: Card) {
-    console.log(area)
-    // if (area instanceof Card) {
-    //   const title = area.get_node('Control/Header/Title') as godot.Label
-    //   console.log(title.text)
-    // }
+  public _on_area_body_entered(body: godot.RigidBody2D) {
+    if (body !== this && this.dragging) {
+      const position = body.get_position() as any
+      this._position = vector2(position.x, position.y + 20)
+    }
   }
 }
 
