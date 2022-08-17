67509f65136a597ba3839e299479786f25600756
wengxiangmin <157215725@qq.com>
2022-08-17 14:25:12 +0800

    chore

--- tscn/card/card.gd
+++ tscn/card/card.gd
@@ -11,14 +11,12 @@ func _on_View_gui_input(event):
 	if event is InputEventMouseButton && event.button_index == BUTTON_LEFT:
 		dragging = event.pressed
 		drag_movement = global_position - get_global_mouse_position()
-
-	if (dragging):
-	  collision_layer = 0
-	  collision_mask = 0
-	else:
-	  collision_layer = 1
-	  collision_mask = 1
-	pass
+		if dragging:
+			collision_layer = 0
+			collision_mask = 0
+		else:
+			collision_layer = 1
+			collision_mask = 1
 
 # 刚体进入
 func _on_Area_body_entered(body):
@@ -29,4 +27,4 @@ func _on_Area_body_exited(body):
   print(body)
 
 func _process(_delta):
-  if dragging: global_position = get_global_mouse_position() + drag_movement
+	if dragging: global_position = get_global_mouse_position() + drag_movement
