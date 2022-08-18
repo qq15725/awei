406de2c423a885b7c79fdbc4b1a351df5d7f7de7
wengxiangmin <157215725@qq.com>
2022-08-18 13:27:13 +0800

    fix: zindex

--- shared/stackable.gd
+++ shared/stackable.gd
@@ -14,12 +14,16 @@ func stack(_node: Node2D):
 		prev = _node
 		if prev.stackable: 
 			prev.stackable.next = node
+		node.z_index = prev.z_index + 1
+		_update_next_z_index()
 
 func unstack(_node: Node2D):
 	if prev && _node.get_instance_id() == prev.get_instance_id():
 		if prev.stackable:
 			prev.stackable.next = null
 		prev = null
+		node.z_index = 0
+		_update_next_z_index()
 
 func _check(_node: Node2D) -> bool:
 	var instance_id = node.get_instance_id()
@@ -30,3 +34,9 @@ func _check_prev(instance_id: int) -> bool:
 		if instance_id == prev.get_instance_id(): return false
 		if prev.stackable: return prev.stackable._check_prev(instance_id)
 	return true
+
+func _update_next_z_index():
+	if next:
+		next.z_index = node.z_index + 1
+		if next.stackable:
+			next.stackable._update_next_z_index()
