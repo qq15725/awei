7c666a1db0e346dfc730e754da46acaeadce9a44
wengxiangmin <157215725@qq.com>
2022-08-19 23:00:48 +0800

    feat: 浆果

--- game/main.gd
+++ game/main.gd
@@ -5,4 +5,5 @@ onready var mouse_pointer := $MousePointer
 func _ready():
 	add_child(load('res://game/farmer/farmer.tscn').instance())
 	add_child(load('res://game/berry_bushes/berry_bushes.tscn').instance())
+	add_child(load('res://game/berry/berry.tscn').instance())
 	add_child(load('res://game/farmer/farmer.tscn').instance())
